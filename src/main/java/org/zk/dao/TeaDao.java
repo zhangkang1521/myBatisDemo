9b9e1da0386cd913f5ce1aaced87c363e6caf68a
zhangkang <zhangkang@lvmama.com>
2019-01-07 09:18:13 +0800

    整合spring，2个FactoryBean

---
+++ src/main/java/org/zk/dao/TeaDao.java
@@ -0,0 +1,8 @@
+package org.zk.dao;
+
+/**
+ * Created by zhangkang on 2019/1/6.
+ */
+public class TeaDao {
+    void a(){};
+}
--- src/test/java/org/zk/SpringMybatisTest.java
+++ src/test/java/org/zk/SpringMybatisTest.java
@@ -3,8 +3,12 @@ package org.zk;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zk.dao.TeaDao;
 import org.zk.dao.UserDao;
 import org.zk.model.User;
 
@@ -18,8 +22,20 @@ public class SpringMybatisTest {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    GenericApplicationContext ctx;
+
+
+
     @Test
     public void test1() {
         User user = userDao.findById(1);
+        System.out.println(user.getUsername());
+    }
+
+    @Test
+    public void testScan() {
+        ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(ctx, true);
+        scanner.scan()
     }
 }
